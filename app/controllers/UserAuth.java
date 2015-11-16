32d06fc17d150731a17ba2c1794c2773f013bdae
alex alarcon <alexalarcon364@gmail.com>
2015-11-16 14:32:56 -0500

    modified userLogin controller

--- app/controllers/UserAuth.java
+++ app/controllers/UserAuth.java
@@ -62,26 +62,5 @@ public class UserAuth extends Controller {
         return ok("This is the user/:id page for user profile of id: " + String.valueOf(id) + "\n");
     }
 
-    //Route: GET /user/login/form
-    //Display the form for user login form
-    public Result loginForm() { return ok(views.html.user.loginform.render()); }
 
-    //Route: POST /user/login
-    //Login user
-    public Result login() {
-        DynamicForm userForm = Form.form().bindFromRequest();
-        String username = userForm.data().get("username");
-        String password = userForm.data().get("password");
-
-        User user = User.find.where().eq("username", username).findUnique();
-
-        if(user != null && user.authenticate(password)) {
-            session("user_id", user.id.toString());
-            flash("success", "Welcome back " + user.userName);
-        } else {
-            flash("error", "Invalid login. Check your username and password.");
-        }
-        //need to implement redirect page after login is confirmed
-        return ok();
-    }
 }
